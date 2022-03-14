1221d4bbc4f104141ddf21ff37df62eb293e3cfb
Damian Kucharski <damiankuchar1@gmail.com>
2022-03-14 13:45:41 +0100

    13:45

---
+++ AS_lab1_gr1/Controllers/LeagueController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AS_lab1_gr1.Controllers
+{
+    public class LeagueController : Controller
+    {
+        private readonly MyDbContext _dbContext;
+
+        public LeagueController(MyDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
